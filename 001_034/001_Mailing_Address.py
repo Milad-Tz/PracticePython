e93eb760a850903aa4c5b8fbae40bf625957cc0f
Milad Tahvilzadeh <miladtahvilzadeh@gmail.com>
2024-09-04 00:45:35 +0330

    commit exercise 001

---
+++ 001_034/001_Mailing_Address.py
@@ -0,0 +1,10 @@
+name = "Milad Tz"
+street_address = "No. 45, Azadi St."
+city = "Tehran"
+postal_code = "121212121"
+country = "Iran"
+
+print(name)
+print(street_address)
+print(f"{city}, {postal_code}")
+print(country)
